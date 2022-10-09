a875962874ba88b7dfe2a5dccc47b787903f5b45
sungmuk1985 <sungmuk1985@gmail.com>
2022-10-09 12:40:24 +0900

    22.10.09

--- ln_test.c
+++ ln_test.c
@@ -16,14 +16,39 @@ int main()
 	return 0;
 }
 */
-
+/*
 #include <stdio.h>
 int main()
 {
 	int a;
-	a = 127;
+	a = 127
 	printf("a의 값은 %d 진수로 %o 입니다. \n", 8, a);
 	printf("a의 값은 %d 진수로 %d 입니다. \n", 10, a);
 	printf("a의 값은 %d 진수로 %x 입니다. \n", 16, a);
 	return 0;
 }
+*/
+
+// 변수형 개념 배우기
+
+#include <stdio.h>
+ int main()
+ {
+ 	int x;
+ 	int y;
+ 	int z;
+
+ 	x = 1;
+ 	y = 2;
+
+ 	z = x + y;
+
+ 	printf("%d\n",z);
+ }
+
+
+
+
+
+
+
